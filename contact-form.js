1ad19fd9544c0625b7ea0451e1f8cf7ed2eefa34
ChillFish8 <Harrison.Burt@softwire.com>
2022-02-14 10:33:31 +0000

    code cleanup

--- contact-form.js
+++ contact-form.js
@@ -1,49 +1,53 @@
-
 let contactUsFormSubmitting = false;
 
 function submitContactUsForm() {
-    if (contactUsFormSubmitting) {
-        return true;
+  if (contactUsFormSubmitting) {
+    return true;
+  }
+  contactUsFormSubmitting = true;
+
+  let body = {
+    name: document.getElementById("contact-us-form-name").value,
+    email: document.getElementById("contact-us-form-email").value,
+    message: document.getElementById("contact-us-form-message").value,
+  };
+
+  setContactUsFormStatus("sending");
+  post(
+    "https://ran8cs5ftc.execute-api.eu-west-2.amazonaws.com/default/techsmiths-contact-form", //TODO need to change this to TechSwitch I think?
+    body,
+    () => {
+      // success
+      setContactUsFormStatus("success");
+      contactUsFormSubmitting = false;
+    },
+    () => {
+      // error
+      setContactUsFormStatus("error");
+      contactUsFormSubmitting = false;
     }
-    contactUsFormSubmitting = true;
-
-    let body = {
-        name : document.getElementById('contact-us-form-name').value,
-        email : document.getElementById('contact-us-form-email').value,
-        message : document.getElementById('contact-us-form-message').value
-    };
-
-    setContactUsFormStatus('sending');
-    post('https://ran8cs5ftc.execute-api.eu-west-2.amazonaws.com/default/techsmiths-contact-form', //TODO need to change this to TechSwitch I think?
-        body,
-        () => { // success
-            setContactUsFormStatus('success');
-            contactUsFormSubmitting = false;
-        },
-        () => { // error
-            setContactUsFormStatus('error');
-            contactUsFormSubmitting = false;
-        }
-    )
+  );
 }
 
 function post(url, data, success, error) {
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", url);
-    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    xhr.onreadystatechange = function () {
-        if(xhr.readyState === 4) {
-            if (xhr.status === 200) {
-                success(xhr.responseText);
-            } else {
-                error();
-            }
-        }
-    };
-    xhr.send(JSON.stringify(data));
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", url);
+  xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        success(xhr.responseText);
+      } else {
+        error();
+      }
+    }
+  };
+  xhr.send(JSON.stringify(data));
 }
 
 function setContactUsFormStatus(status) {
-    document.getElementById('contact-us-form').classList.remove('sending', 'success', 'error');
-    document.getElementById('contact-us-form').classList.add(status);
+  document
+    .getElementById("contact-us-form")
+    .classList.remove("sending", "success", "error");
+  document.getElementById("contact-us-form").classList.add(status);
 }
